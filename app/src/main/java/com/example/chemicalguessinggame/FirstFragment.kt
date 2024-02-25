1d5bc55008ebcac765f27120b6fae1227dfc9c12
bbatdelgerekh <bbatdelgerekh@shipcura.com>
2024-02-25 13:27:29 -0500

    Assignment 2 - Chemical Guessing Game

--- app/src/main/java/com/example/chemicalguessinggame/FirstFragment.kt
+++ app/src/main/java/com/example/chemicalguessinggame/FirstFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.chemicalguessinggame.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -15,14 +15,13 @@ class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
+    private val chemicalList = mutableListOf<String>()
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -32,8 +31,31 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonAdd.setOnClickListener {
+            val chemicalNameText = binding.editTextAdd.text.toString()
+            if (chemicalList.contains(chemicalNameText)) {
+                binding.textviewFirst.text = "Chemical '${chemicalNameText}' is already available"
+            } else {
+                chemicalList.add(chemicalNameText)
+                binding.textviewFirst.text = "Chemical '${chemicalNameText}' added successfully"
+            }
+            binding.editTextAdd.text.clear()
+        }
+
+        binding.buttonGuess.setOnClickListener {
+            val chemicalGuessText = binding.editTextGuess.text.toString()
+            val randomElement = chemicalList.randomOrNull()
+            if (randomElement != null) {
+                val message = if (randomElement == chemicalGuessText) {
+                    "Congratulations! You guessed it right. It was $randomElement"
+                } else {
+                    "Sorry, wrong guess. The correct answer was $randomElement"
+                }
+                binding.textviewFirst.text = message
+            } else {
+                binding.textviewFirst.text = "No chemicals available to guess"
+            }
+            binding.editTextGuess.text.clear()
         }
     }
 
